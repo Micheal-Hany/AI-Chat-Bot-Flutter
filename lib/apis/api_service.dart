96fb277d544679aa7511a8971c1e3864f6491422
Micheal_Hany <81144769+Micheal-Hany@users.noreply.github.com>
2025-02-10 00:06:11 +0200

    Update api_service.dart

--- lib/apis/api_service.dart
+++ lib/apis/api_service.dart
@@ -1,6 +1,6 @@
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
-  static String apiKey = 'AIzaSyAGae8v3zmVZNpsDXFf6BE1NyF4tjMTYxY';
+  static String apiKey = '';
   // static String apiKey = dotenv.env['API_KEY'] ?? 'API_KEY not found';
 }
